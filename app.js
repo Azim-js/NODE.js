52494d5318adc71b4a371452f42a9f1f01b4ba95
azim <az1234im@gmail.com>
2021-06-06 19:09:35 +0530

    modules & require &exports

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+
 console.log("hi i am azim");
+
+let counter=require('./count');
+
+console.log(counter(['hi','my','name','is','azim']));
---
+++ count.js
@@ -0,0 +1,9 @@
+let counter =function(arr){
+    return("the array elements are : "+arr.length);
+}
+
+// console.log(counter(['hari','azim','ummar']));
+
+// exporting the function as a module
+
+module.exports=counter;
