38d95ef9444ef516256ef2b6bba061b1b2cb5ead
azim <az1234im@gmail.com>
2021-06-07 01:46:25 +0530

    utils module to make custom events

--- app3.js
+++ app3.js
@@ -14,3 +14,33 @@ myEmmiter.on('someEvent',function(mssg){
 
 // call
 myEmmiter.emit('someEvent','the Event mssg was emmitted');
+
+
+// using utils module to inherit the object into
+
+let util=require('util');
+
+let person=function(name)
+{
+    this.name=name;
+};
+
+// custom events using utils
+
+util.inherits(person,events.EventEmitter);  //creating an event
+
+let azim=new person('azim');
+let hari=new person('hari');
+let ummar =new person('ummar');
+
+let people=[hari,azim,ummar] ;
+
+people.forEach(function(person){
+    person.on('speak',function(mssg){
+        console.log(person.name+' said :'+mssg);
+    })
+})
+
+hari.emit('speak','hi i am backend developer');
+azim.emit('speak',"hello i am working on node.js");
+ummar.emit('speak',"hello i am fullstack developer");
