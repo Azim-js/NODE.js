bfdcbc044418eecc47364bf4e046ff607ebe6d24
azim <az1234im@gmail.com>
2021-06-09 16:35:16 +0530

    async method to write and read contents form file

--- app4.js
+++ app4.js
@@ -4,8 +4,25 @@ const fs=require('fs');
 
 // reading and writing content of a file usinf fs.sync
 
-var content=fs.readFileSync('readme.txt','utf8');
+let content=fs.readFileSync('readme.txt','utf8');
 
 console.log(content);
 
 fs.writeFileSync('writeMe.txt',content);
+
+// reading and writting to a file using fs. async which is quiker than sync method
+
+let mainData=fs.readFile('async.txt','utf8',function(err,data){
+    console.log(data);
+});
+
+console.log("to see where this gets loaded");  //before the async method calls
+
+fs.readFile('async.txt','utf8',function(err,data){
+    // console.log(data);
+    fs.writeFile('asyncWrite.txt',data,function(err,result){
+        if(err){
+            console.log("error: "+ err);
+        }
+    });
+});
