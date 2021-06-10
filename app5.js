aca6a35750159d9e28c051583d753d9cff8b650e
azim <az1234im@gmail.com>
2021-06-10 12:59:10 +0530

    creating directories using async callback

--- app5.js
+++ app5.js
@@ -2,12 +2,24 @@
 const fs=require('fs');
 
 // sync method
-fs.unlinkSync('writeMe.txt');
+// fs.unlinkSync('writeMe.txt');
 
 // creating directories
 
 // sync method
 
-fs.mkdirSync('stuff'); //to make dirctory
+// fs.mkdirSync('stuff'); //to make dirctory
 
 // fs.rmdirSync('stuff'); // to remove dir
+
+// ASYNC methods to create and remove uisng callback functions 
+
+fs.mkdir('stuff',function(){
+    fs.readFile('readme.txt','utf-8',function(err,data){
+        fs.writeFile('./stuff/writeMe.txt',data,function(err,data){
+            if(err){
+                console.log('error: '+err);
+            }
+        });
+    });
+});
