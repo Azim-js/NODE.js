76adad5c85382bd1428b49c801514895909ff8a4
azim <az1234im@gmail.com>
2021-06-10 12:49:29 +0530

    to create mkdir and rmdir using sync

---
+++ app5.js
@@ -0,0 +1,13 @@
+// to create and remove directories
+const fs=require('fs');
+
+// sync method
+fs.unlinkSync('writeMe.txt');
+
+// creating directories
+
+// sync method
+
+fs.mkdirSync('stuff'); //to make dirctory
+
+// fs.rmdirSync('stuff'); // to remove dir
