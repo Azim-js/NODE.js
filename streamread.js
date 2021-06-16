4f61a31e30f60114e4509476817b0119f3329430
azim <az1234im@gmail.com>
2021-06-16 17:52:40 +0530

    added encoding 'utf-8'

--- streamread.js
+++ streamread.js
@@ -2,7 +2,8 @@ const http = require('http');
 
 const fs=require('fs'); //to readthe file in the form of streams 
 
-const streamdata=fs.createReadStream(__dirname+'/chunk.txt');
+const streamdata=fs.createReadStream(__dirname+'/chunk.txt','utf-8'); //using 8 bit unicode to english and numericals
+
 
 // creates an event with name 'data' to listen 
 
