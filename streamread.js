290a9f458398b955f7d68736521aebd9ea96523a
azim <az1234im@gmail.com>
2021-06-17 13:33:44 +0530

    stream write using  .write()

--- streamread.js
+++ streamread.js
@@ -2,13 +2,22 @@ const http = require('http');
 
 const fs=require('fs'); //to readthe file in the form of streams 
 
-const streamdata=fs.createReadStream(__dirname+'/chunk.txt','utf-8'); //using 8 bit unicode to english and numericals
+const readStreamData=fs.createReadStream(__dirname+'/chunk.txt','utf-8'); //using 8 bit unicode to english and numericals
+
+//reading data in chuncks and wriTTING DATA IN chuncks
+
+
+const writeStreamData=fs.createWriteStream(__dirname+'/writechunk.txt','utf-8');
 
 
 // creates an event with name 'data' to listen 
 
 
-streamdata.on('data',function(chunck){
- console.log("the chunck received are :");
- console.log(chunck);
+readStreamData.on('data',function(chunck){
+ console.log("new Chunck recived :");
+//  console.log(chunck);
+// simul writing when reading the buffer chuncks as stream
+
+writeStreamData.write(chunck);
+
 });
