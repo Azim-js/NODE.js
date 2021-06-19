90cb092e2a1458d233ab5fd09cf35a06fc54d5c9
azim <az1234im@gmail.com>
2021-06-19 17:08:27 +0530

    adding conatct.html path

--- basicROuting.js
+++ basicROuting.js
@@ -8,9 +8,13 @@ const server=http.createServer(function(req,res){
         res.writeHead(200,{'Content-Type':'text/html'});
         fs.createReadStream(__dirname+"/index.html").pipe(res);
     }
+    else if(req.url==='/contact'){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        fs.createReadStream(__dirname+"/contact.html").pipe(res);
+    }
 
 });
 
 server.listen(3000,'127.0.0.1');
 
-console.log("listenong at port : 3000");
+console.log("listening at port : 3000");
