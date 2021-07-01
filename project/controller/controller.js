8a6c6ece2b16efcf31a1b201e80c730e8fb4c345
azim <az1234im@gmail.com>
2021-07-01 18:37:13 +0530

    dynamic addition and deletion from mongodb (done)

--- project/controller/controller.js
+++ project/controller/controller.js
@@ -1,4 +1,4 @@
-let data=[{item:"Milk"},{item:"Eggs"},{item:"Bread"},{item:"Chocolate"}];
+// let data=[{item:"Milk"},{item:"Eggs"},{item:"Bread"},{item:"Chocolate"}];
 
 const mongoose=require('mongoose');
 
@@ -16,14 +16,14 @@ const todoSchema=new mongoose.Schema({
 
 const todo=mongoose.model('todo',todoSchema);
 
-// adding on to the db
+// adding on to the db dummy data
 
-let itemOne=todo({item:"Buy Flowers"}).save(function(err){
-    if(err){
-        throw err;
-    }
-    console.log("item Saved");
-})
+// let itemOne=todo({item:"Buy Flowers"}).save(function(err){
+//     if(err){
+//         throw err;
+//     }
+//     console.log("item Saved");
+// })
 
 const bodyParser=require('body-parser');
 
@@ -31,27 +31,50 @@ const urlencodedParser=bodyParser.urlencoded({extended:false});
 
 module.exports=function(app){
 
+    // get request 
+
     app.get("/todo",function(req,res){
-        res.render("todo",{item:data});
+        // get data and display it {} means all 
+        todo.find({},function(err,data){
+            if(err){
+                throw err;
+            }
+            res.render("todo",{item:data});
+        });
+        
     });
+
+
+
+    // post request after submitting the form
+
     app.post("/todo",urlencodedParser,function(req,res){
-        console.log(req.body);
-        data.push(req.body);
-        // send the data in json format 
-        res.json(data);
-        // res.redirect("/todo");
-        // res.render("todo",{item:data});
+        // get data from the view and add it to the mongodb
+
+        let newToDo=todo(req.body).save(function(err,data){
+            if(err){
+                throw err;
+            }
+            // data.push(req.body);
+            // send the data in json format 
+            res.json(data);
+            
+    
+        })
+
+        
     
     });
+
+    // delete request
     app.delete("/todo/:item",function(req,res){
-        data=data.filter(function(todo){
-            // replace the click list item whit blank space
-            return todo.item.replace(/ /g,'-')!== req.params.item;
-
+    //    find the item and remove it form mongodb
+        todo.find({item:req.params.item.replace(/\-/g,' ')}).remove(function(err,data){
+            if(err){
+                throw err;
+            }
+            res.json(data);
         });
-
-        res.json(data);
-
 
     });
 }
